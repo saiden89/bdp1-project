6f3c41b79a03d9de5218a34fb7c0d771de580092
Stefano Roncelli <45285915+saiden89@users.noreply.github.com>
2021-02-06 12:30:29 +0100

    Update install_kubeadm.sh

--- script/install_kubeadm.sh
+++ script/install_kubeadm.sh
@@ -1,3 +1,14 @@
+# Letting iptables see bridged traffic
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
 # (Install containerd)
 sudo apt update
 sudo apt install -y containerd
