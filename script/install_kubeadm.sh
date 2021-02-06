c5050e3b6e7b1f089975f45fbb114ec9b48e9dd6
Stefano Roncelli <45285915+saiden89@users.noreply.github.com>
2021-02-06 12:45:07 +0100

    Update install_kubeadm.sh

--- script/install_kubeadm.sh
+++ script/install_kubeadm.sh
@@ -49,18 +49,3 @@ EOF
 sudo apt update
 sudo apt install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-
-# Initialize master
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16
-
-# Configure kubectl
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# Autocompletion
-echo 'source <(kubectl completion bash)' >>~/.bashrc
-source ~/.bashrc
-
-kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
-kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
