7c092364ed3c5a24c197e51945c87bfdf589c14b
Stefano Roncelli <45285915+saiden89@users.noreply.github.com>
2021-02-06 12:23:00 +0100

    add autocompletion

--- script/install_kubeadm.sh
+++ script/install_kubeadm.sh
@@ -42,9 +42,14 @@ sudo apt-mark hold kubelet kubeadm kubectl
 # Initialize master
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 
+# Configure kubectl
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Autocompletion
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+source ~/.bashrc
 
 kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
 kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
