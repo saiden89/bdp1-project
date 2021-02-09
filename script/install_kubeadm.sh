7991edd3350e8984b5d452a2133388775441f557
Stefano Roncelli <45285915+saiden89@users.noreply.github.com>
2021-02-09 17:59:34 +0100

    Update install_kubeadm.sh

--- script/install_kubeadm.sh
+++ script/install_kubeadm.sh
@@ -1,19 +1,4 @@
-# Letting iptables see bridged traffic
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sudo sysctl --system
-
-# (Install containerd)
-sudo apt update
-sudo apt install -y containerd
-
-# Set containerd
+# Set containerd prerequisites
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
@@ -32,6 +17,10 @@ EOF
 # Apply sysctl params without reboot
 sudo sysctl --system
 
+# Install containerd
+sudo apt update
+sudo apt install -y containerd
+
 # Configure containerd
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
@@ -39,8 +28,19 @@ containerd config default | sudo tee /etc/containerd/config.toml
 # Restart containerd
 sudo systemctl restart containerd
 
-#Install Runtime
-sudo apt update
+# Kubernetes
+# Letting iptables see bridged traffic
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+# Install Runtime
 sudo apt install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
