45826924d90d02ae1e40482eb06eebf26d1335c2
Stefano Roncelli <45285915+saiden89@users.noreply.github.com>
2021-02-09 17:02:24 +0100

    Update kubeadm_init.sh

--- script/kubeadm_init.sh
+++ script/kubeadm_init.sh
@@ -6,12 +6,12 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+# Calico
+kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+
 # Autocompletion
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
-
 source ~/.bashrc
-
-kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
-kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
