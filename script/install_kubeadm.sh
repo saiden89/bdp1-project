1205fa13035b7fcfbe4ead9353f08f63f2d0a774
Stefano Roncelli <45285915+saiden89@users.noreply.github.com>
2021-02-10 18:22:05 +0100

    Update install_kubeadm.sh

--- script/install_kubeadm.sh
+++ script/install_kubeadm.sh
@@ -17,19 +17,7 @@ EOF
 # Apply sysctl params without reboot
 sudo sysctl --system
 
-# Install containerd
-sudo apt update
-sudo apt install -y containerd
-
-# Configure containerd
-sudo mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
-
-# Restart containerd
-sudo systemctl restart containerd
-
-# Kubernetes
-# Letting iptables see bridged traffic
+# Kubernetes - Letting iptables see bridged traffic
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
 EOF
@@ -47,5 +35,12 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+sudo apt install -y kubelet kubeadm kubectl containerd
 sudo apt-mark hold kubelet kubeadm kubectl
+
+# Configure containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+
+# Restart containerd
+sudo systemctl restart containerd
